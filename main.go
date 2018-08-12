17f88b9a1d06ea175dd1ffd5c1804ac6ac193169
Wuvist <wuvist@gmail.com>
2018-08-12 21:26:11 +0800

    Add css static

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/Wuvist/goblog/static"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo"
 	"github.com/volatiletech/sqlboiler/boil"
 )
+
+//go:generate esc -o static/css.go -pkg static -include="(.+).css" -prefix="oldweb/" oldweb/Template
 
 func main() {
 	// db
@@ -25,6 +28,9 @@ func main() {
 	e.GET("/blogger", blogger)
 	e.GET("/cate", cate)
 	e.GET("/blog", blog)
+
+	// static
+	e.GET("/Template/*", echo.WrapHandler(http.FileServer(static.FS(false))))
 
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
