6df6c43ca5aca3fba1d2fc93c28212cdd8f0524e
Weng Wei <wuvist@gmail.com>
2025-10-30 10:44:05 +0800

    cache

--- tpl/skins/skin5_UserCate.go
+++ tpl/skins/skin5_UserCate.go
@@ -29,7 +29,7 @@ func RenderSkin5_UserCate(_buffer io.StringWriter, blogger *tpl.Blogger, cate *t
 	// Line: 10
 	_buffer.WriteString(gorazor.HTMLEscStr(blogger.BlogName))
 	// Line: 10
-	_buffer.WriteString("</title>\n<link rel=\"stylesheet\" href=\"/Template/skin5.css?t=0\" type=\"text/css\"> \n<script type=\"text/javascript\" src=\"/icon.js\"></script>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n</head>\n<body bgcolor=\"ffffff\" marginwidth=\"0\" marginheight=\"0\">\n\n<table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n<tr bgcolor=\"#ffffff\">\n<td class=\"name\"  width=\"100%\" height=\"60\">")
+	_buffer.WriteString("</title>\n<link rel=\"stylesheet\" href=\"/Template/skin5.css?t=1\" type=\"text/css\"> \n<script type=\"text/javascript\" src=\"/icon.js\"></script>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n</head>\n<body bgcolor=\"ffffff\" marginwidth=\"0\" marginheight=\"0\">\n\n<table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\">\n<tr bgcolor=\"#ffffff\">\n<td class=\"name\"  width=\"100%\" height=\"60\">")
 	// Line: 20
 	_buffer.WriteString(gorazor.HTMLEscStr(blogger.BlogName))
 	// Line: 20
